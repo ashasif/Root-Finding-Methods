fce9bd3760771579bddd0689f72e27fc2dda0a4a
ashasif <ashasif090@gmail.com>
2018-10-11 10:06:33 +0000

    graph not showing

--- secant.py
+++ secant.py
@@ -1,37 +1,50 @@
+import sympy as sym
+import matplotlib.pyplot as plt
+a=[]
+b=[]
+c=[]
+
+
+
 def y(x):
 
-    return  x**2 - 5*x + 6
-
+    return  (x**2-5*x+4)
     #  return x**3-x**2-26*x-24
 
     #  return x*(sin(3*x)- 3*sin(3*x)
 
     #  return 5*e**x -sin(x)-x**2
 
-x1=int(input('x1?'))
+for i in range (-20,21):
+    x1=i
+    x0=x1-1
+    count=0
+    while (abs(x1-x0))>0.00000000000000001:
+        p=(y(x1)-y(x0))
+        if(p==0):
+            break
+        else:
+            x2=x1-((x1-x0)*y(x1)/(y(x1)-y(x0)))
+            x0=x1
+            x1=x2
+        count+=1
+        #print('x2 is',x2)
+    print('for i=',i,'The root is ',x2)
+    print('steps taken',count)
+    #print('i is ',i)
+    #print('y is',y)
+    #print('steps are',count)
 
-x0=x1-1
+    a.append(i)
+    b.append(y)
+    c.append(count)
 
-count=0
+print('a[]=',a)
+print('b[]=',b)
+print(c)
+#
+plt.plot(a,b)
+plt.show()
+plt.plot(a,c)
+plt.show()
 
-print(x0)
-
-print(x1)
-
-
-while (abs(x1-x0))>0.00000000000000001:
-
-    x2=x1-((x1-x0)*y(x1)/(y(x1)-y(x0)))
-
-    x0=x1
-
-    x1=x2
-
-    count+=1
-
-    print('x2 is',x2)
-
-
-print('The root is ',x2)
-
-print('steps taken',count)
